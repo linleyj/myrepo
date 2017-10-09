768a38471d4bb7db443709bdbbdbbeb5832d4d56
mguillaudeux <morgan.guillaudeux35@gmail.com>
2017-10-10 11:48:28 +1300

    Use the daily data function to get the graph

---
+++ Daily graph/Radiation Plot.R
@@ -0,0 +1,142 @@
+#installation of necessary packages
+
+knitr::opts_chunk$set(echo = F, cache=T)
+library(pacman)
+p_load(devtools,httr,roxygen2,RCurl,dplyr)
+library(dplyr);library(XML)
+library(RCurl);library(selectr);library(clifro)
+library(ggplot2)
+library(tidyverse)
+library(forcats)
+library(lubridate)
+
+#proxy setup
+
+options(RCurlOptions = list(proxy = 'http://proxy.pfr.co.nz:8080')) 
+cf_curl_opts(.opts = list(proxy = "https://proxy.pfr.co.nz:8080", ssl.verifypeer = FALSE))
+
+#username and password for Cliflo database
+
+me = cf_user("linleyj","PFRCLIFLO")
+
+#plot theme
+
+theme_linley <- function(){
+  theme_grey(base_size = 15, base_family = "") %+replace% 
+  theme(panel.background = element_rect(fill = "white", 
+  colour = NA), panel.border = element_rect(fill = NA, 
+  colour = "grey20"), panel.grid.major = element_line(colour = "grey92"), 
+  panel.grid.minor = element_line(colour = "grey92", 
+  size = 0.25), strip.background = element_rect(fill = "grey85", 
+  colour = "black"), legend.key = element_rect(fill = "white",                                                                                                                                    colour = NA), complete = TRUE)
+}
+
+#first function is doing the queries
+
+
+DATES<-function(start_D,end_D,Number_year,STATION,DATATYPE1,DATATYPE2,DATATYPE3)
+{
+  
+  #set up the new start date for the long term query
+  
+  start_D2 <- as.Date(start_D) - years(Number_year)
+  print(start_D2)
+  
+  #Queries
+  
+  New.datalist  <-  cf_query(user = me, 
+                             station =  cf_station(STATION), datatype = cf_datatype(DATATYPE1,DATATYPE2,DATATYPE3),
+                             start_date = start_D,
+                             end_date = end_D)
+  
+  LTNew.datalist  <-  cf_query(user = me, 
+                               station =  cf_station(STATION), datatype = cf_datatype(DATATYPE1,DATATYPE2,DATATYPE3),
+                               start_date = start_D2,
+                               end_date = end_D)
+  
+  #turning lists into data frames
+  
+  Newdata_df <- New.datalist %>% 
+    map_df(`[`)
+  
+  LTNewdata_df <- LTNew.datalist %>% 
+    map_df(`[`)
+  
+  #Change head of columns to avoid any problem
+  
+  names(LTNewdata_df)[names(LTNewdata_df)=="Date(local)"] <- "DDate"
+  names(LTNewdata_df)[names(LTNewdata_df)=="Amount(MJ/m2)"] <- "Amount"
+  names(Newdata_df)[names(Newdata_df)=="Date(local)"] <- "DDate"
+  names(Newdata_df)[names(Newdata_df)=="Amount(MJ/m2)"] <- "Amount"
+  
+  #Find NA's
+  
+  LT_sum <- LTNewdata_df %>%
+    mutate(Year=format(DDate, "%Y")) %>% 
+    mutate(Monthyear=format(DDate, "%Y-%b")) %>% 
+    mutate(MonthDay=format(DDate, "%b-%d")) %>% 
+    group_by(MonthDay) %>%
+    filter(is.na(Amount)) %>%
+    summarise(total =sum(Amount))
+  
+  out <- list(LTNewdata_df=LTNewdata_df, Newdata_df=Newdata_df)
+  print(str(out))
+  return(out)
+}
+
+DATES(start_D= "2012-03-01 00", end_D="2013-08-01 00" ,Number_year= 10,STATION = "3925",DATATYPE1 =  5,DATATYPE2 =  2, DATATYPE3 =  1)
+
+
+
+PLOT<- function(startDay,endDay,numberYear)
+{
+  Data10years_df <-   as.tibble(DATES(start_D = startDay, end_D = endDay, Number_year =  numberYear, STATION = "3925", 5,2,1)$LTNewdata_df)
+  head(Data10years_df)
+  start_D2 <- as.Date(startDay) - years(numberYear)
+  print(start_D2)
+  
+  dfstep<-Data10years_df %>%
+    ungroup() %>%
+    mutate(DDate=as.Date(DDate)) %>% 
+    ungroup() %>%
+    arrange(DDate) %>% 
+    mutate(ordDate=format(DDate, "%j")) %>% 
+    mutate(MonthDay=as.factor(format(DDate, "%b-%d")))%>%
+    mutate(MonthDay=fct_inorder(MonthDay)) %>% 
+    group_by(ordDate) %>% 
+    summarise(MEANBYDAY=mean(Amount,na.rm=TRUE))
+  
+  
+  dataperiod_df <- as.tibble(DATES(start_D = startDay, end_D = endDay, Number_year =  numberYear, STATION = "3925", 5,2,1)$Newdata_df)
+  print(summary(dataperiod_df$DDate))
+  
+  df_join <- dataperiod_df %>% 
+    mutate(ordDate=format(DDate, "%j")) %>% 
+    full_join(.,dfstep, by="ordDate") %>% 
+    arrange(DDate) %>% 
+    ungroup() %>% 
+    mutate(cumAmount=cumsum(MEANBYDAY))
+  
+  Check<<-df_join
+  Check2<<-dfstep
+  
+  
+  ggplot(data = df_join, aes(x=as.Date(DDate),y=Amount)) +
+    geom_area(fill="purple",show.legend = FALSE,alpha=0.4) +
+    geom_point(aes(x=as.Date(DDate),y=cumsum(Amount/100)),color="red")+
+    geom_path(aes(x=as.Date(DDate), y=cumsum(Amount/100),group=1),color="red")+
+    geom_point(aes(x=as.Date(DDate), y=cumAmount/100,group=1),color="blue")+
+    xlab("Date")+
+    ylab("Daily Radiation (MJ/m2)")+
+    theme_linley()+
+    scale_x_date(date_labels = "%Y-%b")+
+    scale_y_continuous("Cumulative radiation (MJ/m2)", sec.axis = sec_axis(~.*100, name = derive()))
+  
+}
+
+PLOT(startDay =  "2009-03-01 00", endDay =  "2013-08-01 00", numberYear =   10)
+
+
+
+
+
